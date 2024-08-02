cced84c87c1e2202fafc3fd39ccea19a0f4bb000
Jalen Brown <jbrown36@umbc.edu>
2024-08-02 15:56:01 -0400

    Update planet_simulation.py

--- planet_simulation.py
+++ planet_simulation.py
@@ -5,17 +5,18 @@ from enum import Enum
 pygame.init()
 
 # Set pygame window
-WIDTH, HEIGHT = 700, 650
+WIDTH, HEIGHT = 1000, 650
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))  # PI game surface aka window
 pygame.display.set_caption('Planet Simulation')  # Window title
 
 FONT = pygame.font.SysFont("comicsans", 16)  # Create font
+FONT2 = pygame.font.SysFont("comicsans", 24)  # Create font
 
 # TODO: Add a add planet button (by order)
 # TODO: Add a remove planet button (by order)
 # TODO: Add a speed slider
-# TODO: Add a title (top of window)
 # TODO: Add a side navigation
+
 
 class Color(Enum):
     WHITE = (255, 255, 255)
@@ -50,14 +51,14 @@ class Planet:
         self.y_vel = 0
 
     def draw(self, win):
-        x = self.x * self.SCALE + WIDTH / 2
+        x = (self.x * self.SCALE + WIDTH / 2) + 300
         y = self.y * self.SCALE + HEIGHT / 2
 
         if len(self.orbit) >= 2:
             updated_points = []
             for point in self.orbit:
                 point_x, point_y = point
-                point_x = point_x * self.SCALE + WIDTH / 2
+                point_x = (point_x * self.SCALE + WIDTH / 2) + 300
                 point_y = point_y * self.SCALE + HEIGHT / 2
                 updated_points.append((point_x, point_y))
 
@@ -68,7 +69,8 @@ class Planet:
 
         if not self.sun:
             distance_text = FONT.render(
-                f'{round(self.distance_to_sun/1000, 1)}km', True, Color.WHITE.value)  # Create text object
+                # Create text object
+                f'{round(self.distance_to_sun/1000, 1)}km', True, Color.WHITE.value)
             win.blit(distance_text, (x - distance_text.get_width()/2,
                      y - distance_text.get_height()/2))  # Render text on screen
 
@@ -106,6 +108,20 @@ class Planet:
         self.y += self.y_vel * self.TIMESTEP
         self.orbit.append((self.x, self.y))
 
+class SideBar:
+    def __init__(self, x, y, width, height, color) -> None:
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+    
+    def draw(self, win):
+        x = self.x
+        y = self.y + HEIGHT / 2
+        
+        pygame.draw.rect(win, self.color, (x, y, self.width, self.height))
+
 # Game loop
 
 
@@ -113,6 +129,8 @@ def main():
     run = True
     clock = pygame.time.Clock()  # Create clock instance (help with synchronization)
 
+    sidebar = SideBar(16, 0, WIDTH/3, HEIGHT * 2/3, Color.WHITE.value)
+    
     sun = Planet(0, 0, 30, Color.YELLOW.value, 1.989 * 10**30, True)
     earth = Planet(-1 * Planet.AU, 0, 16,
                    Color.BLUE.value, 5.972 * 10**24, False)
@@ -136,12 +154,19 @@ def main():
         clock.tick(60)  # Set frame rate (update)
         WIN.fill((0, 0, 0))
 
+        title = FONT2.render('Planet Simulation', True,
+                             Color.WHITE.value)  # Create text object
+        # Render text on screen
+        WIN.blit(title, ((WIDTH - title.get_width())/2, 16))
+
         # Get events (e.g. keyboard, mouse, etc.)
         for event in pygame.event.get():
             # Close event
             if event.type == pygame.QUIT:
                 run = False
 
+        sidebar.draw(WIN)
+        
         for planet in planets:
             planet.update_position(planets)
             planet.draw(WIN)
